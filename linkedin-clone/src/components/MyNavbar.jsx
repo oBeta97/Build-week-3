5e1ea2060ae9926b9c9c5724c02cba6e7ba9a1f2
mary1457 <piabaldoin@gmail.com>
2024-09-09 16:28:30 +0200

    modifiche navbar

--- linkedin-clone/src/components/MyNavbar.jsx
+++ linkedin-clone/src/components/MyNavbar.jsx
@@ -4,7 +4,6 @@ import {
   Nav,
   Form,
   FormControl,
-  Button,
   NavDropdown,
   Container,
 } from "react-bootstrap";
@@ -21,54 +20,60 @@ import { CgMenuGridO } from "react-icons/cg";
 
 export const MyNavbar = () => {
   return (
-    <Navbar
-      bg="light"
-      expand="lg"
-      className="sticky-top px-5 d-flex justify-content-center align-items-center"
-    >
-      <Navbar.Brand href="#home">
-        <FaLinkedin color="#0077b5" size="2rem" />
-      </Navbar.Brand>
+    <Navbar bg="light" expand="lg" className="sticky-top px-4 py-2">
+      <Container>
+     
+        <Navbar.Brand href="#home" className="d-flex align-items-center">
+          <FaLinkedin color="#0077b5" size="2rem" />
+        </Navbar.Brand>
 
-      <Form inline className="mx-auto  d-none d-lg-flex ">
-        <FaSearch className="me-2" />
-        <FormControl type="text" placeholder="Search" className="w-100 gap-4" />
-      </Form>
-      <Container className=" ps-5">
-        <Navbar.Toggle aria-controls="basic-navbar-nav " />
-        <Navbar.Collapse id="basic-navbar-nav ">
-          <Nav className="ml-auto  ">
-            <Nav.Link href="#home">
-              <FaHome className="me-1" />
+        <Form className="d-flex flex-grow-1 mx-4 d-none d-md-flex align-items-center">
+      <FaSearch className="me-2" style={{ fontSize: '1.25rem' }} />
+      <FormControl type="text" placeholder="Search" className="w-100" />
+    </Form>
+
+      
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto d-flex align-items-center">
+            <Nav.Link href="#home" className="d-flex align-items-center ">
+              <FaHome className="me-1 "  />
               Home
             </Nav.Link>
-            <Nav.Link href="#network">
+            <Nav.Link href="#network" className="d-flex align-items-center ">
               <FaUserFriends className="me-1" />
               My Network
             </Nav.Link>
-            <Nav.Link href="#jobs">
+            <Nav.Link href="#jobs" className="d-flex align-items-center ">
               <FaBriefcase className="me-1" />
               Jobs
             </Nav.Link>
-            <Nav.Link href="#messaging">
+            <Nav.Link href="#messaging" className="d-flex align-items-center ">
               <FaEnvelope className="me-1" />
               Messaging
             </Nav.Link>
-            <Nav.Link href="#notifications">
+            <Nav.Link href="#notifications" className="d-flex align-items-center ">
               <FaBell className="me-1" />
               Notifications
             </Nav.Link>
+            
+          
             <NavDropdown title="Me" id="basic-nav-dropdown">
               <NavDropdown.Item href="#profile">Profile</NavDropdown.Item>
               <NavDropdown.Item href="#settings">Settings</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#logout">Sign Out</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#apps" className="text-center border-start">
-              <CgMenuGridO className=" me-2 " />
+            
+            
+            <Nav.Link href="#apps" className="text-center border-start d-none d-xl-flex align-items-center ps-3">
+              <CgMenuGridO className="me-2" />
               Per le aziende
             </Nav.Link>
-            <Nav.Link>Prova premium per 0 EURO</Nav.Link>
+            <Nav.Link className="d-none d-xl-flex align-items-center">
+              Prova premium per 0 EURO
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
