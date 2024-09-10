17049ff499db602fae3621f89067bb753fa1bbd3
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-09-10 10:37:40 +0200

    1

--- linkedin-clone/src/App.jsx
+++ linkedin-clone/src/App.jsx
@@ -2,17 +2,16 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import MyFooter from './components/MyFooter'
+import MyFooter from "./components/MyFooter";
 import { MyNavbar } from "./components/MyNavbar";
 import { Col, Container, Row } from "react-bootstrap";
 import ProfilePage from "./components/ProfilPage";
-
+import CardComponent from "./components/CardComponent";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
-
         <header className="bg-light p-0">
           <Container>
             <Row className="justify-content-center">
