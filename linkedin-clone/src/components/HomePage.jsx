daeeda13242b7ed621211c7635e26726a30acc61
oBeta97 <paolopellizzari97@gmail.com>
2024-09-11 12:19:29 +0200

    Aggiunta modalità lite al componente MeCard per la visualizzazione nella home page

--- linkedin-clone/src/components/HomePage.jsx
+++ linkedin-clone/src/components/HomePage.jsx
@@ -1,12 +1,13 @@
 import MyBaseContainer from "./MyBaseContainer";
 import ThreeCol from "./ThreeCol";
+import MeCard from "./MeCard"
 
 
 const HomePage = () => {
     return (
         <MyBaseContainer>
             <ThreeCol 
-                left=""
+                left={<MeCard lite /> }
                 center=""
                 right=""
             />
--- linkedin-clone/src/components/MeCard/ProfileInfo.jsx
+++ linkedin-clone/src/components/MeCard/ProfileInfo.jsx
@@ -5,32 +5,47 @@ import { FaUserCheck } from "react-icons/fa";
 const ProfileInfo = (props) => {
     return (
         <Row>
-            <Col xs={12} lg={8}>
+            <Col xs={12} lg={props.lite ? 12 : 8}>
                 <div className="d-flex flex-column flex-md-row align-items-start gap-2">
                     <h3 className="p-0 m-0">{`${props.profileData.name} ${props.profileData.surname}`}</h3>
-                    <div
-                        className="
-                        border-2 border-dashed border-primary rounded-pill
-                        text-primary
-                        px-3 ms-1
-                        d-flex justify-content-between align-items-center gap-1
-                    "
-                    >
-                        <FaUserCheck />
-                        <span >Verifica ora!</span>
-                    </div>
+                    {
+                        !props.lite ? (
+                            <div
+                                className="
+                                    border-2 border-dashed border-primary rounded-pill
+                                    text-primary
+                                    px-3 ms-1
+                                    d-flex justify-content-between align-items-center gap-1
+                                "
+                            >
+                                <FaUserCheck />
+                                <span >Verifica ora!</span>
+                            </div>
+                        ) : ""
+                    }
+
                 </div>
                 <p className="text-start p-0 m-0">{props.profileData.username}</p>
                 {props.profileData.area ? (
-
-                    <p className="text-start text-secondary p-0 m-0">{props.profileData.area} ·
-                        <span className="text-primary">Informazioni di contatto</span>
+                    <p className="text-start text-secondary p-0 m-0">{props.profileData.area}
+                        {
+                            !props.lite ? (
+                                <>
+                                    · <span className="text-primary">Informazioni di contatto</span>
+                                </>
+                            ) : ""
+                        }
                     </p>
                 ) : ""
                 }
-                <p className="text-start text-secondary fw-bold mb-1"> {props.profileData.bio}</p>
+
+                {
+                    !props.lite ? (
+                        <p className="text-start text-secondary fw-bold mb-1"> {props.profileData.bio}</p>
+                    ) : ""
+                }
             </Col>
-            <Col xs={12} lg={4}> 
+            <Col xs={12} lg={4}>
                 <div className="d-flex align-items-center gap-2 mb-1">
                     <img
                         src="https://media.licdn.com/dms/image/v2/C4E0BAQHYgix-Ynux1A/company-logo_100_100/company-logo_100_100/0/1646830188798/epicodeschool_logo?e=1733961600&v=beta&t=nrLWWjKHe6sHcWyfi1ckaHtt3l7c2mBfc1Oz3-0T1ZU"
