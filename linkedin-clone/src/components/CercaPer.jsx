30bcc00115958e3f3392cea0fa6c557d428eb02a
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-09-12 12:43:28 +0200

    three col

---
+++ linkedin-clone/src/components/CercaPer.jsx
@@ -0,0 +1,25 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+const CercaPer = () => {
+  return (
+    <Container className="bg-white border rounded ">
+      <Row>
+        <Col xs={12}>
+          <h4 className="text-start"> Cerca Per:</h4>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <p className="text-start ">Azienda</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <p className="text-start ">categoria</p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default CercaPer;
--- linkedin-clone/src/components/SearchPage.jsx
+++ linkedin-clone/src/components/SearchPage.jsx
@@ -1,8 +1,13 @@
-
-
+import ThreeCol from "./ThreeCol";
+import MyBaseContainer from "./MyBaseContainer";
+import CercaPer from "./CercaPer";
 
 const SearchPage = () => {
-    return ("");
-}
+  return (
+    <MyBaseContainer>
+      <ThreeCol left={<CercaPer />} center="" right="" />
+    </MyBaseContainer>
+  );
+};
 
 export default SearchPage;
