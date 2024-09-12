b824693e585f11ecda81433bbadd2cfe911f54c4
campion-luca <campionluca3@gmail.com>
2024-09-12 16:02:07 +0200

    complete the card that pop up when you search for something but it's static bec i'm  dump and i dont know inglish
    by luca

--- linkedin-clone/src/components/SearchPage.jsx
+++ linkedin-clone/src/components/SearchPage.jsx
@@ -1,8 +1,29 @@
-
-
+import { Col, Container, Row } from "react-bootstrap";
+import SearchCard from "./SearchCard";
 
 const SearchPage = () => {
-    return ("");
-}
+  return (
+    <Container>
+      <Row>
+
+        {/* PRIMA COLONNA */}
+        <Col xs={3}>
+        </Col>
+
+
+        <Col xs={6}>
+          <SearchCard />
+          {/* POSTO RISERVATO AL COMPONENTE DI LUCA */}
+        </Col>
+
+
+        {/* TERZA COLONNA */}
+        <Col xs={3}>
+        </Col>
+
+      </Row>
+    </Container>
+  );
+};
 
 export default SearchPage;
