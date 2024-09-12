871de6e5987eef04606f1d01a9db8b0124979eb0
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-09-12 12:44:06 +0200

    1

--- linkedin-clone/src/components/CercaPer.jsx
+++ linkedin-clone/src/components/CercaPer.jsx
@@ -10,7 +10,7 @@ const CercaPer = () => {
       </Row>
       <Row>
         <Col xs={12}>
-          <p className="text-start ">Azienda</p>
+          <p className="text-start ">Aziendea</p>
         </Col>
       </Row>
       <Row>
