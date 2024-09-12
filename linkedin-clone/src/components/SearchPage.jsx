51a8bb4f3824fd0f7144651d786cc44ac609305e
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-09-12 13:00:14 +0200

    sistemato destra sinistra del search

--- linkedin-clone/src/components/SearchPage.jsx
+++ linkedin-clone/src/components/SearchPage.jsx
@@ -2,10 +2,12 @@ import ThreeCol from "./ThreeCol";
 import MyBaseContainer from "./MyBaseContainer";
 import CercaPer from "./CercaPer";
 
+import HomePageRightAside from "./HomePageRightAside";
+
 const SearchPage = () => {
   return (
     <MyBaseContainer>
-      <ThreeCol left={<CercaPer />} center="" right="" />
+      <ThreeCol left={<CercaPer />} center="" right={<HomePageRightAside />} />
     </MyBaseContainer>
   );
 };
